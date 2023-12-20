fab222813c2fbd102c6ef8d6a3b02ad49dcbcb05
Patryk <krasowski.p@outlook.com>
2023-12-20 20:21:14 +0100

    handlong loading states

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,14 +2,17 @@ import { useEffect, useState } from 'react'
 import Places from './Places.jsx'
 
 export default function AvailablePlaces({ onSelectPlace }) {
+	const [isFetching, setIsFetching] = useState(false)
 	const [availablePlaces, setAvailablePlaces] = useState([])
 
 	useEffect(() => {
 		async function fetchPlaces() {
 			try {
+				setIsFetching(true)
 				const response = await fetch('http://localhost:3000/places')
 				const data = await response.json()
 				setAvailablePlaces(data.places)
+				setIsFetching(false)
 			} catch {}
 		}
 		fetchPlaces()
@@ -19,6 +22,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
 		<Places
 			title='Available Places'
 			places={availablePlaces}
+			isLoading={isFetching}
+			loadingText='Fetching place data...'
 			fallbackText='No places available.'
 			onSelectPlace={onSelectPlace}
 		/>
