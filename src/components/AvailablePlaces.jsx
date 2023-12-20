5ee1ae18bbf8aed1bf71d41706f42fe1827c9cd8
Patryk <krasowski.p@outlook.com>
2023-12-20 20:14:39 +0100

    added useEffect and fetchPlaces fn

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,26 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from 'react'
+import Places from './Places.jsx'
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+	const [availablePlaces, setAvailablePlaces] = useState([])
+
+	useEffect(() => {
+		async function fetchPlaces() {
+			try {
+				const response = await fetch('http://localhost:3000/places')
+				const data = await response.json()
+				setAvailablePlaces(data.places)
+			} catch {}
+		}
+		fetchPlaces()
+	}, [])
+
+	return (
+		<Places
+			title='Available Places'
+			places={availablePlaces}
+			fallbackText='No places available.'
+			onSelectPlace={onSelectPlace}
+		/>
+	)
 }
